99b967965d27c5f370c47f1f0925270edbcbe130
elfredaonwenna2021-lab <elfredaonwenna2021@gmail.com>
2026-02-02 21:01:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+// Select the button
+const enrollBtn = document.getElementById("enrollBtn");
+
+// Add click event
+enrollBtn.addEventListener("click", function () {
+  alert("🎉 Welcome to NexaDev Academy! Enrollment opens soon.");
+});
